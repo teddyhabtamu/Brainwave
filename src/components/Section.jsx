ecbee2d09942cacbba5dcf2205ef03ea86f8f8fa
teddyhabtamu <tewodroshabtamu29@gmail.com>
2024-10-09 20:57:42 +0300

    Header Finished

---
+++ src/components/Section.jsx
@@ -0,0 +1,11 @@
+
+const Section = ({className, id, crosses, crossesOffset, customPaddings, children}) => {
+  return (
+    <div className={`relative ${customPaddings || 'py-10 lg:py-16 xl:py-20'} ${crosses ? 'lg:py-32 xl:py-40': ''} ${className || ''}`}>
+      {children}
+      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none"/>
+      </div>
+  )
+}
+
+export default Section
